542f8b082d2ef80209434ed78f3d04eac28c3fbf
aleksandr leonov <aleksandr.leonov@iteco.com>
2025-12-26 14:58:06 +0300

    stage

--- internal/infra/http/server/module.go
+++ internal/infra/http/server/module.go
@@ -1,10 +1,13 @@
 package server
 
 import (
+	"context"
 	"destinations-suggester/internal/infra/http/server/handlers/suggestions"
 	"destinations-suggester/internal/pkg/fxutils"
+	"destinations-suggester/internal/pkg/sl"
 	"github.com/labstack/echo/v4"
 	"go.uber.org/fx"
+	"log/slog"
 )
 
 var Module = fx.Module("http.server",
@@ -18,6 +21,22 @@ var Module = fx.Module("http.server",
 		fxutils.Register[*suggestions.Lister](),
 	),
 	fx.Invoke(
-		fxutils.Append[*Server](),
+		func(lc fx.Lifecycle, sh fx.Shutdowner, srv *Server) {
+			lc.Append(fx.Hook{
+				OnStart: func(ctx context.Context) error {
+					go func() {
+						if err := srv.Start(ctx); err != nil {
+							slog.Error("cannot start http server", sl.Error(err))
+							if err := sh.Shutdown(); err != nil {
+								slog.Error("cannot shutdown application", sl.Error(err))
+							}
+						}
+					}()
+
+					return nil
+				},
+				OnStop: srv.Stop,
+			})
+		},
 	),
 )
--- internal/infra/http/server/server.go
+++ internal/infra/http/server/server.go
@@ -2,50 +2,36 @@ package server
 
 import (
 	"context"
-	"destinations-suggester/internal/pkg/sl"
 	"fmt"
 	"github.com/labstack/echo/v4"
-	"log/slog"
 	"net"
 	"net/http"
 )
 
 type Server struct {
-	logger *slog.Logger
-	srv    *echo.Echo
-	cancel context.CancelCauseFunc
-	conf   *Config
+	srv  *echo.Echo
+	conf *Config
 }
 
-func New(srv *echo.Echo, cancel context.CancelCauseFunc, conf *Config) *Server {
+func New(srv *echo.Echo, conf *Config) *Server {
 	return &Server{
-		logger: sl.WithComponent("http.server.Server"),
-		srv:    srv,
-		cancel: cancel,
-		conf:   conf,
+		srv:  srv,
+		conf: conf,
 	}
 }
 
 func (s *Server) Start(_ context.Context) error {
-	go func() {
-		err := s.srv.StartServer(&http.Server{
-			Addr:              net.JoinHostPort(s.conf.Host, s.conf.Port),
-			ReadTimeout:       s.conf.ReadTimeout,
-			ReadHeaderTimeout: s.conf.ReadHeaderTimeout,
-			WriteTimeout:      s.conf.WriteTimeout,
-			IdleTimeout:       s.conf.IdleTimeout,
-			MaxHeaderBytes:    s.conf.MaxHeaderBytes,
-		})
-		if err != nil {
-			s.logger.Error("cannot start http server", sl.Error(err))
-			s.cancel(err)
-		}
-
-		s.logger.Info("http server started",
-			slog.Any("host", s.conf.Host),
-			slog.Any("port", s.conf.Port),
-		)
-	}()
+	err := s.srv.StartServer(&http.Server{
+		Addr:              net.JoinHostPort(s.conf.Host, s.conf.Port),
+		ReadTimeout:       s.conf.ReadTimeout,
+		ReadHeaderTimeout: s.conf.ReadHeaderTimeout,
+		WriteTimeout:      s.conf.WriteTimeout,
+		IdleTimeout:       s.conf.IdleTimeout,
+		MaxHeaderBytes:    s.conf.MaxHeaderBytes,
+	})
+	if err != nil {
+		return fmt.Errorf("cannot start http server: %w", err)
+	}
 
 	return nil
 }
